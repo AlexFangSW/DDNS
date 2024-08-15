f2d1b51165a4393cc7b2d0dbc5afae8e50426f2e
AlexFang <alexfangsw@gmail.com>
2024-08-15 17:13:51 +0800

    chore: update formatting and use cache from our registory

--- main.go
+++ main.go
@@ -121,16 +121,16 @@ func updateDNSRecord() error {
 }
 
 func main() {
-  for { 
-    log.Println("start updating dns record")
+	for {
+		log.Println("start updating dns record")
 
-    if err := updateDNSRecord(); err != nil {
-      log.Fatal(err)
-    }
+		if err := updateDNSRecord(); err != nil {
+			log.Fatal(err)
+		}
 
-    log.Println("finish updating dsn record")
+		log.Println("finish updating dsn record")
 
-    log.Println("sleeping for 5 minutes...")
-  	time.Sleep(5 *time.Minute)
-  }
+		log.Println("sleeping for 5 minutes...")
+		time.Sleep(5 * time.Minute)
+	}
 }
