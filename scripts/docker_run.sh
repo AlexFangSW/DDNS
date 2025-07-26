62fa3556cf05c66a0b514cd026a71884079adfdd
AlexFangSW <alexfangsw@gmail.com>
2025-07-26 06:06:53 +0000

    chore: set fixed ip for docker container
    prevent collision with my other long lived containers that needs
    to have a fixed ip

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -7,5 +7,6 @@ set +a
 docker run -d --name ddns \
   --env-file $ENV_DIR/.env.ddns \
   --network infra  \
-  --restart=unless-stopped \
+	--restart=always \
+  --ip 172.20.0.3 \
   "$DOCKER_REPO/$DOCKER_PROJECT/$DOCKER_IMAGE:$DOCKER_TAG"
